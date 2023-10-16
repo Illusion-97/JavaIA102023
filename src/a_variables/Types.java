5800118b4e5487ff784c7d3541a48af77be40f40
Yanis Adekalom <yanis971.60@gmail.com>
2023-10-16 11:02:02 +0200

    Fix : Types

--- src/a_variables/Types.java
+++ src/a_variables/Types.java
@@ -26,8 +26,30 @@ public class Types {
         // endregion
 
         // region REFERENCE
-        String aString = null;          //
-        aString = "String Content";     // pour la saisie de texte : " pour les String et ' pour les char
-        System.out.println("Valeur avant replace : " + aString);
+        /* un type réference se comporte à la manière d'un pointeur, initialement il n'y a pas de taille mémoire prédéfinie
+        il faudra donc toujours affecter une valeur à une déclaration de type primitif
+        un type reference va pointer vers une ou plusieurs valeurs de type primitives (ou également réference)
+        afin que la taille affectée en mémoire corresponde uniquement au types primitifs nécessaires au stockage de l'information
+         */
+        String aString = null;                                          // Par défaut un String est immutable
+        aString = "String Content";                                     // pour la saisie de texte : " pour les String et ' pour les char
+        System.out.println("Valeur avant replace : " + aString);        // Valeur avant replace : String Content
+        aString.replace(" ", "");                       // Une modification de la valeur de l'objet n'est pas automatiquement réaffectée
+        System.out.println("Valeur après replace : " + aString);        // Valeur après replace : String Content
+        aString = aString.replace(" ", "");             // Pour que la modification de la valeur soit effective, je dois la réassigner à la variable déclarée
+        System.out.println("Valeur après réaffectation : " + aString);  // Valeur après réaffectation : StringContent
+
+        Object parent = null; // Toutes les classes héritent automatiquement de la classe Object, classe qui elle même est un type reference
+        // enregion
+
+        // region WRAPPER
+        Integer anIntWrapper = 0;                                   // int anInt = 0;
+        int anIntFromWrapper = new Integer(0);                // Je peux créer un Objet de type réference par son contructeur eu utiliser cet objet directement dans un type primitif
+        int anotherIntFromWrapper = anIntWrapper;                   // La conversion entre un wrapper et ton type primitif est automatique
+        int anIntFromValueOf = Integer.valueOf(0);
+        int anIntFromStringValueOf = Integer.valueOf("0");      // Ecriture est dépréciée
+        int anIntFromStingParseInt = Integer.parseInt("0");     // Utilie pour valider des saisies utilisateur
+        // endregion
+
     }
 }

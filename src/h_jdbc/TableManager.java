681269aa10b673abd6cec10ef4d5de8f23405cdc
Yanis Adekalom <yanis971.60@gmail.com>
2023-10-20 14:07:03 +0200

    Fix : JDBC

--- src/h_jdbc/TableManager.java
+++ src/h_jdbc/TableManager.java
@@ -2,12 +2,23 @@ package h_jdbc;
 
 import h_jdbc.dao.IPersonneDAO;
 import h_jdbc.dao.PersonneDAOImpl;
+import h_jdbc.models.Personne;
+import h_jdbc.models.Role;
 
 public class TableManager {
     public static void main(String[] args) {
         try {
             IPersonneDAO personneDAO = new PersonneDAOImpl();
             personneDAO.createTable();
+            Personne yanis = new Personne(0L, 0, "ADEKALOM", "Yanis", Role.ADMIN);
+            personneDAO.create(yanis);
+            System.out.println(yanis);
+            System.out.println("personneDAO.findById(4) = " + personneDAO.findById(4));
+            System.out.println("personneDAO.findById(25) = " + personneDAO.findById(25));
+            System.out.println("Find All : ");
+            personneDAO.findAll().forEach(System.out::println);
+            yanis.setRole(Role.CLIENT);
+            personneDAO.update(yanis);
         } catch (Exception e) {
             e.printStackTrace(System.out);
         }
--- src/h_jdbc/dao/PersonneDAOImpl.java
+++ src/h_jdbc/dao/PersonneDAOImpl.java
@@ -2,8 +2,10 @@ package h_jdbc.dao;
 
 import h_jdbc.DbConnection;
 import h_jdbc.models.Personne;
+import h_jdbc.models.Role;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonneDAOImpl implements IPersonneDAO {
@@ -18,7 +20,8 @@ public class PersonneDAOImpl implements IPersonneDAO {
     public boolean create(Personne personne) {
         // PreparedStatement prévoit une syntaxe permettant de prévenir l'injection SQL
         try (PreparedStatement preparedStatement = connection.prepareStatement(
-                "INSERT INTO Personne(version,nom,prenom,role) VALUES(?,?,?,?)"
+                "INSERT INTO Personne(version,nom,prenom,role) VALUES(?,?,?,?)",
+                Statement.RETURN_GENERATED_KEYS // demande à ma requete de me retourner les id générés
         )) {
             // A l'aide de différentes fonctions pour chaque type souhaité, preparedStatement vérifie l'intégrité de la donnée avant de la passer à la requête
             preparedStatement.setInt(1, personne.getVersion());
@@ -26,6 +29,10 @@ public class PersonneDAOImpl implements IPersonneDAO {
             preparedStatement.setString(3, personne.getPrenom());
             preparedStatement.setString(4, personne.getRole().name());
             preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys(); // Un ResultSet représente le retour d'une requête SQL (Tableau avec des colonnes et des données sur chaque ligne)
+            if(resultSet.next()) { // si j'ai bien une donnée à la ligne suivante
+                personne.setId(resultSet.getLong(1)); //
+            }
             return true;
         } catch (SQLException e) {
             e.printStackTrace(System.out);
@@ -35,27 +42,76 @@ public class PersonneDAOImpl implements IPersonneDAO {
 
     @Override
     public Personne findById(long id) {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT * FROM Personne WHERE id = ?"
+        )) {
+            preparedStatement.setLong(1,id); // Toujours passer par une vérification quel que soit le type de variable
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            return mapResultToPersonne(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
         return null;
     }
 
     @Override
     public List<Personne> findAll() {
-        return null;
+        List<Personne> personnes = new ArrayList<>();
+        try(Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Personne");
+            while (resultSet.next()) personnes.add(mapResultToPersonne(resultSet));
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+        return personnes;
     }
 
     @Override
     public boolean update(Personne personne) {
-        return false;
+        try(PreparedStatement preparedStatement = connection.prepareStatement(
+                "UPDATE Personne SET version = ?, nom = ?, prenom = ?, role = ? WHERE id = ? AND version = ?"
+        )) {
+            preparedStatement.setInt(1, personne.getVersion() + 1);
+            preparedStatement.setString(2, personne.getNom());
+            preparedStatement.setString(3, personne.getPrenom());
+            preparedStatement.setString(4, personne.getRole().name());
+            preparedStatement.setLong(5,personne.getId());
+            preparedStatement.setInt(6, personne.getVersion());
+            return preparedStatement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+            return false;
+        }
     }
 
     @Override
     public boolean delete(long id) {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(
+                "DELETE FROM Personne WHERE id = ?"
+        )) {
+            preparedStatement.setLong(1,id);
+            preparedStatement.executeQuery();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
         return false;
     }
 
     @Override
     public Personne mapResultToPersonne(ResultSet resultSet) {
-        return null;
+        try {
+            return new Personne(
+                    resultSet.getLong("id"),
+                    resultSet.getInt("version"),
+                    resultSet.getString("nom"),
+                    resultSet.getString("prenom"),
+                    Role.valueOf(resultSet.getString("role"))
+            );
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
